6cff49e7cf8a0d3fe8670a6172673e1b9e3b816b
adam <valentine.bellius@gmail.com>
2025-06-07 17:03:19 +0300

    ExtCrawler dir:'No edits /' // .gitignore:'No edits' // ExtCrawler.py:'No edits' // analyzer.py:'No edits' // hangar dir'No edits' // sattelite.py:'Refactored / Updated version marker'

--- hangar/sattelite.py
+++ hangar/sattelite.py
@@ -16,34 +16,32 @@ class Sattelite:
         self.ext = ext
         self.directory = os.getcwd()
 
+    def get_files(self):
+        files = glob.glob(self.ext, recursive=True)
+        return files
+
+    def logger_knocker(self, files):
+        logger = logging.getLogger('knocker')
+        d = {'folder': self.directory}
+
+        count = len(files)
+        match count:
+            case 1:
+                verb = 'is a'
+            case 0:
+                verb = 'is no any'
+            case _:
+                verb = 'are a'
+
+        logger.warning(f'There {verb} {self.ext}: %s',
+                       f' Quantity #{count}: {files}',
+                       extra=d)
+
     def knocker(self):
-
-            files = glob.glob(self.ext, recursive=True)
-
-            logger = logging.getLogger('knocker')
-            d = {'folder': self.directory}
-
-            count = 0
-            files_arr = []
-            for file in files:
-
-                if any(file):
-                    count += 1
-                    files_arr.append(file)
-
-            match count:
-                case 1:
-                    verb = 'is a'
-                case 0:
-                    verb = 'is no any'
-                case _:
-                    verb = 'are a'
-
-            logger.warning(f'There {verb} {self.ext}: %s',
-                           f' Quantity #{count}: {files_arr}',
-                           extra=d)
-
-            return files_arr
+        files = self.get_files()
+        self.logger_knocker(files)
+        return files, self.directory
 
 if __name__ == "__main__":
-    Sattelite().knocker()
+    sat = Sattelite()
+    files, folder = sat.knocker()
